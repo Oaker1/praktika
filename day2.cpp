97be08a87a2b410759ab38e0a627c010cdec7ca6
Oaker1 <72872932+Oaker1@users.noreply.github.com>
2021-06-30 18:50:02 +0300

    Add files via upload

---
+++ day2.cpp
@@ -0,0 +1,14 @@
+
+
+
+int CompareMas(int* source, int* dest, unsigned int size)
+{
+    return 0;
+}
+
+int main()
+{
+    unsigned int size = 4;
+    int source[] = {1,3,5,7};
+    int dest[] = {1,3,5,6};
+}
